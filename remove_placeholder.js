dacbb09dcfedc6301eb2cba49bf9cc888e2cc1fe
kerollos <kerolloz@yahoo.com>
2019-06-21 18:46:29 +0200

    fix remove placeholder

--- remove_placeholder.js
+++ remove_placeholder.js
@@ -1,8 +1,8 @@
 function remove_placeholder() {
-    skills_length = document.getElementsByTagName("li").length;
+    skills_length = document.getElementsByTagName("li").length - document.getElementsByTagName("js-template").length;
     skills_input = document.getElementById("question-input-101");
 
-    if (skills_length === 1)
+    if (skills_length === 0)
         skills_input.placeholder = "Space separated";
     else
         skills_input.placeholder = "";
