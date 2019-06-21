5c879bbd03fe774d9867e8816f18083e273f6696
kerollos <kerolloz@yahoo.com>
2019-06-21 14:27:56 +0200

    ES5 compatible submit

--- submit.js
+++ submit.js
@@ -1,18 +1,17 @@
 function parse() {
-    let name = document.getElementById("add-user").elements[0].value
-    let email = document.getElementById("add-user").elements[1].value
-    let skills = document.getElementsByTagName("li");
-    let user_skills = [];
+    name = document.getElementById("add-user").elements[0].value;
+    email = document.getElementById("add-user").elements[1].value;
+    skills = document.getElementsByTagName("li");
+    user_skills = [];
     if (skills.length > 1) {
-        for (let i = 1; i < skills.length; i++)
+        for (i = 1; i < skills.length; i++)
             user_skills.push(skills[i].innerHTML.split('\n')[1].trim());
 
-        let data = {
+        data = {
             name: name,
             email: email,
             skills: user_skills
-        }
-        console.log(JSON.stringify(data))
+        };
         fetch("http://localhost:5000/users", {
                 method: 'POST',
                 // mode: 'cors', // no-cors, cors, *same-origin
@@ -29,11 +28,9 @@ function parse() {
             .then(response => response.json())
             .then(response => console.log(response))
             .catch(err => console.log(err));
-
     } else {
         alert("Please add your skills first");
     }
-    console.log(user_skills);
     event.preventDefault();
 
 }
