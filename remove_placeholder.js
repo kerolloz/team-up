8825d2378bd80dcc92f44bbbb703262e94b5d0f1
kerollos <kerolloz@yahoo.com>
2019-07-07 05:36:40 +0200

    remove debugging in console

--- remove_placeholder.js
+++ remove_placeholder.js
@@ -1,6 +1,5 @@
 function remove_placeholder(_placeholder = "Space separated") {
     const skills_length = $("form li").not('.js-template').length;
-    console.log(skills_length)
     const skills_input = document.getElementById("question-input-101");
 
     if (skills_length === 0)
