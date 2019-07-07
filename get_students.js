0eef56a9443a616c3c6628c870680d998f27a849
kerollos <kerolloz@yahoo.com>
2019-07-07 04:37:02 +0200

    add basic functionality for searching

--- get_students.js
+++ get_students.js
@@ -1,53 +1,39 @@
-function get_all_students() {
-    loading()
-    fetch("https://api-team-up-fci.herokuapp.com/users")
-        .then(response => response.json())
-        .then(students => show_students(students.docs))
-        .catch(err => show_nothing());
-}
+//  get students by name 
+$('#name-form').submit(function (e) {
+    e.preventDefault();
+    const section_id = 'students-by-name';
+    loading(section_id)
+    const name = $("input[name=name]").val();
 
-function search() {
-    if (document.getElementById("search-form").elements[0].value !== "") {
-        if (document.getElementById("search-form").elements[1].checked) // name
-            get_students_by_name();
-        else // skills
-            get_students_by_skills();
-    } else {
-        get_all_students();
-    }
-    event.preventDefault();
-}
-
-function toggler(element) {
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
-
-function toggle_style() {
-    const skills_input = document.getElementById("skills-input");
-    const name_input = document.getElementById("name-input");
-    toggler(skills_input);
-    toggler(name_input);
-}
-
-function get_students_by_name() {
-    loading()
-    const name = document.getElementById("search-form").elements[0].value;
-    console.log(name)
     fetch(`https://api-team-up-fci.herokuapp.com/users?name=${name}`)
         .then(response => response.json())
-        .then(students => show_students(students))
-        .catch(err => show_nothing());
-}
+        .then(students => show_students(students, 'students-by-name'))
+        .catch(err => show_nothing(section_id));
+})
 
-function get_students_by_skills() {
+// get students by skills
+$('#skills-form').submit(function (e) {
+    e.preventDefault();
+    const section_id = 'students-by-skills';
+    loading(section_id)
+    console.log("skills");
 
-}
+    const skills = document.getElementsByTagName("li");
+    if (skills.length > 1) {
+        let user_skills = [];
+        for (i = 1; i < skills.length; i++)
+            user_skills.push(skills[i].innerHTML.split('\n')[1].trim());
 
-function show_students(students_list) {
+        fetch(`https://api-team-up-fci.herokuapp.com/users?skills=${user_skills.join(',')}`)
+            .then(response => response.json())
+            .then(students => show_students(students, section_id))
+            .catch(err => show_nothing(section_id));
+    } else {
+        alert("Please add skills first!");
+    }
+})
+
+function show_students(students_list, section_id) {
     if (students_list.length === 0)
         return show_nothing();
 
@@ -58,29 +44,40 @@ function show_students(students_list) {
             skills += `<li class="topic-tag-action f6 p-2 float-left js-tag-input-tag text-blue" style="height: 25px">${skill}</li>`
         }
         list_in_html += `<div class="ui fluid container mb-2 p-2 text-white rounded student" >
-        Name: ${student.name}<br>
-        Email: ${student.email}
+        <i class="portrait icon"></i> ${student.name}<br>
+        <i class="envelope icon"></i> 
+        <a href="mailto: ${student.email}" class="text-white" style="  text-decoration: underline;">${student.email}</a>
         <ul class=" js-tag-input-selected-tags">${skills}</ul></div>`;
     }
-    set_search_results_html(list_in_html);
+    set_search_results_html(list_in_html, section_id);
 }
 
-function show_nothing() {
-    set_search_results_html("<div class='h3 text-red'> Nothing found </div>");
-}
-
-function set_search_results_html(html_content) {
-    document.getElementById("students-list").innerHTML = html_content;
-}
-
-function loading() {
-    set_search_results_html(`<div id="students-list" class="col- mt-4">
-    <div class="ui blue swinging indeterminate progress">
-        <div class="bar">
-            <div class="progress">Searching</div>
+function show_nothing(section_id) {
+    set_search_results_html(`
+    <div class="ui icon message red">
+        <i class="user alternate slash icon"></i>
+        <div class="content">
+        <div class="header">
+            Opss!
+        </div>
+        <p>Nothing found!</p>
         </div>
     </div>
-</div>`);
+    `, section_id);
 }
 
-get_all_students(); // when the page load get all students 
+function set_search_results_html(html_content, section_id) {
+    document.getElementById(section_id).innerHTML = html_content;
+}
+
+function loading(section_id) {
+    set_search_results_html(`<div class="ui icon message">
+    <i class="notched circle loading icon"></i>
+    <div class="content">
+      <div class="header">
+        Just one second
+      </div>
+      <p>We're fetching that content for you.</p>
+    </div>
+  </div>`, section_id);
+}
