36327090e56e6a8f8d8cb57cd5215703f8d21111
kerollos <kerolloz@yahoo.com>
2019-07-07 05:33:01 +0200

    finally fix the freakin' placeholder issue

--- remove_placeholder.js
+++ remove_placeholder.js
@@ -1,10 +1,10 @@
-function remove_placeholder() {
-    skills_length = document.getElementsByTagName("li").length - document.getElementsByTagName("js-template").length;
-    skills_input = document.getElementById("question-input-101");
+function remove_placeholder(_placeholder = "Space separated") {
+    const skills_length = $("form li").not('.js-template').length;
+    console.log(skills_length)
+    const skills_input = document.getElementById("question-input-101");
 
     if (skills_length === 0)
-        skills_input.placeholder = "Space separated";
+        skills_input.placeholder = _placeholder;
     else
         skills_input.placeholder = "";
-
 }
