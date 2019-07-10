4553a9535ef99ef649d895fa85e27265e87ee992
kerollos <kerolloz@yahoo.com>
2019-07-11 00:56:02 +0200

    enhance info page styling
    - let js handle segment coloring 
    - let js handle segment id

---
+++ index.js
@@ -0,0 +1,25 @@
+(function () {
+    let id = 0;
+    const segments = $(".segment").toArray();
+    const colors = [
+        "Red",
+        "Orange",
+        "Yellow",
+        "Olive",
+        "Green",
+        "Teal",
+        "Blue",
+        "Violet",
+        "Purple",
+        "Pink",
+        "Brown",
+        "Grey",
+        "Black"
+    ]
+    segments.forEach(element => {
+        element.classList.add(colors[id].toLowerCase())
+        element.id = ++id;
+
+    });
+
+})();
