ec5eac70fc1fdb96da78c130e8e6224ae13a3b5f
Kerollos Magdy <kerolloz@yahoo.com>
2020-05-13 22:53:31 +0200

    refactor api requests service

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,49 +3,50 @@ interface User {
   email: string;
   skills: string[];
 }
+
 interface ErrorResponse {
   message: string;
   errors: { label: string; type: string; message: string }[];
 }
 
 const api = {
+  BASE_URI: 'https://api-team-up-fci.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
-  async post(uri: string, body: {}) {
-    return await fetch(uri, {
+  async post(endpoint: string, body: {}) {
+    return await fetch(this.BASE_URI + endpoint, {
       method: 'post',
       headers: this.headers,
       body: JSON.stringify(body),
     });
   },
-  async get(uri: string) {
-    return await fetch(uri);
+  async get(endpoint: string) {
+    return await fetch(this.BASE_URI + endpoint);
   },
-  async delete(uri: string) {
-    return await fetch(uri, {
+  async delete(endpoint: string) {
+    return await fetch(this.BASE_URI + endpoint, {
       method: 'delete',
     });
   },
 };
 
 export default {
-  BASE_URI: 'https://api-team-up-fci.herokuapp.com',
   async signup(body: User) {
-    return await api.post(`${this.BASE_URI}/users`, body);
+    return await api.post('/users', body);
   },
   async verify(token: string) {
-    return await api.post(`${this.BASE_URI}/users/verify/${token}`, {});
+    return await api.post(`/users/verify/${token}`, {});
   },
   async remove(token: string) {
-    return await api.delete(`${this.BASE_URI}/users/${token}`);
+    return await api.delete(`/users/${token}`);
   },
   async findByName(name: string) {
-    return await api.get(`${this.BASE_URI}/users/?name=${name}`);
+    return await api.get(`/users/?name=${name}`);
   },
   async findBySkills(skills: string[]) {
     const s = skills.map((e) => `skills=${e}`).join('&');
-    return await api.get(`${this.BASE_URI}/users/?${s}`);
+    return await api.get(`/users/?${s}`);
   },
   parseErrors(body: ErrorResponse) {
     return body.errors.map((e) => `${e.label.split('.')[1]}: ${e.message}`);
