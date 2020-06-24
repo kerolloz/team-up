f8f16413b1f0835a9b186d66a840aa7af4a666fd
Kerollos Magdy <kerolloz@yahoo.com>
2020-06-24 15:45:57 +0200

    simple refactor for student card

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="mb-2">
-    <div class="rounded-2 p-2 bg-blue text-white text-center width-full">
+    <div class="rounded-2 p-2 bg-blue text-white text-center">
       <div class="mt-2">
-        <h3 class="">{{ name }}</h3>
+        <h3 class="">{{ student.name }}</h3>
       </div>
       <div class="mt-2">
-        <a :href="`mailto:${email}`" class="text-white text-underline f4">{{
-          email
-        }}</a>
+        <a
+          :href="`mailto:${student.email}`"
+          class="text-white text-underline f4"
+          >{{ student.email }}</a
+        >
       </div>
       <div class="mt-3">
         <span
           class="Label bg-blue-light text-gray-dark mr-1 mb-1 p-2 text-normal f4"
-          v-for="skill in skills"
+          v-for="skill in student.skills"
           :key="skill"
         >
           {{ skill }}
@@ -25,9 +27,5 @@
 <script>
 export default {
   props: ['student'],
-  data() {
-    const { name, email, skills } = this.student;
-    return { name, email, skills };
-  },
 };
 </script>
