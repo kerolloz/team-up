2c352dd288b04c3469f0c861ab1a209108b0e539
Kerollos Magdy <kerolloz@yahoo.com>
2022-07-28 20:10:16 +0000

    format NavigationBar.vue

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,15 +1,9 @@
 <template>
   <!-- Navbar -->
   <div class="Header bg-blue flex-justify-between mb-5 p-1">
-    <NavigationItem to="/registration">
-      Register
-    </NavigationItem>
-    <NavigationItem to="/" class="h3">
-      TEAM UP
-    </NavigationItem>
-    <NavigationItem to="/students">
-      Students
-    </NavigationItem>
+    <NavigationItem to="/registration"> Register </NavigationItem>
+    <NavigationItem to="/" class="h3"> TEAM UP </NavigationItem>
+    <NavigationItem to="/students"> Students </NavigationItem>
   </div>
 </template>
 
